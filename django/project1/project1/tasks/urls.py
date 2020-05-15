eb59b2161feb6e672bd9299c3c162bb1a720b9bb
Neeraj Kumar Gupta <neeraj.gupta@netcracker.com>
2020-05-15 13:28:19 +0530

    --- Auto-Git Commit ---

--- django/project1/project1/tasks/urls.py
+++ django/project1/project1/tasks/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("index", views.index, name="index"),
+    path("add", views.add, name="add")
 
 ]
