43eb278903a47a759ab98b71cc7b2ff6d41b7998
Neeraj Kumar Gupta <neeraj.gupta@netcracker.com>
2020-05-15 12:28:07 +0530

    --- Auto-Git Commit ---

--- django/project1/project1/tasks/urls.py
+++ django/project1/project1/tasks/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', include("hello.urls")),
-    path('newYear/', include("newyear.urls")),
-    path('tasks/', include("tasks.urls"))
+    path("", views.index, name="index"),
+
 ]
